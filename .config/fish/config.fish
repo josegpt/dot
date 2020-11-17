5f6e176e9a61db2940f46ab508ae77a81b192942
Jose G Perez Taveras <josegpt27@gmail.com>
2020-11-16 20:55:40 -0500

    removed fish and also transfered emacs config to org mode

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-# boostrap fisher
-if not functions -q fisher
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fish -c fisher
-end
-
-# alias for dotfiles
-alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
