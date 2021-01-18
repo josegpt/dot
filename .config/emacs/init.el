31c2b3c5df2cf73600f293971c8ed27006f79042
Jose G Perez Taveras <josegpt27@gmail.com>
2021-01-18 10:24:04 -0500

    remove ketcast from autogenerated code

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -390,7 +390,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(pass keycast exwm which-key monokai-theme rainbow-delimiters multiple-cursors move-text magit diminish counsel ivy-rich ivy expand-region desktop-environment use-package)))
+   '(pass exwm which-key monokai-theme rainbow-delimiters multiple-cursors move-text magit diminish counsel ivy-rich ivy expand-region desktop-environment use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
