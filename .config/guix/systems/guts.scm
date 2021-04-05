da385a885b16ae0f46a8779e9920a1ff1830ddf9
Jose G Perez Taveras <josegpt27@gmail.com>
2021-04-04 20:28:15 -0400

    guix...

---
+++ .config/guix/systems/guts.scm
@@ -0,0 +1,18 @@
+(define-module (guts)
+  #:use-module (base-system)
+  #:use-module (gnu))
+
+(operating-system
+ (inherit base-operating-system)
+ (host-name "guts")
+
+ (file-systems (cons*
+                (file-system
+                 (mount-point "/boot/efi")
+                 (device (uuid "1234-ABCD" 'fat32))
+                 (type "vfat"))
+                (file-system
+                 (mount-point "/")
+                 (device (file-system-label "system-root"))
+                 (type "ext4"))
+                %base-file-systems)))
