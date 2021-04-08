0dd986b1c77f0c05cdea5cfcaf1c34f5dc30fe64
Jose G Perez Taveras <josegpt27@gmail.com>
2021-04-08 19:21:50 -0400

    [Guix] casca

--- .config/guix/systems/casca.scm
+++ .config/guix/systems/casca.scm
@@ -9,14 +9,13 @@
  (bootloader
   (bootloader-configuration
    (bootloader grub-bootloader)
-   (target "/dev/sda1")
+   (target "/dev/sda")
    (keyboard-layout keyboard-layout)))
 
+ (swap-devices (list
+                (file-system-label "swap")))
+
  (file-systems (cons*
-                (file-system
-                 (mount-point "/boot/efi")
-                 (device (uuid "1234-ABCD" 'fat32))
-                 (type "vfat"))
                 (file-system
                  (mount-point "/")
                  (device (file-system-label "system-root"))
