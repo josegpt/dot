1964c4d737426decc91452c10422ba280aa448c5
Jose G Perez Taveras <josegpt27@gmail.com>
2021-11-17 21:38:01 -0500

    update[emacs]: Update weeb.el

--- .config/emacs/elisp/weeb.el
+++ .config/emacs/elisp/weeb.el
@@ -1,7 +1,7 @@
 ;;; weeb.el --- Search and watch anime from Emacs -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2021 Jose G Perez Taveras <josegpt27@gmail.com>
-;; Version: 0.1
+;; Version: 0.9
 
 ;; Permission is hereby granted, free of charge, to any person obtaining
 ;; a copy of this software and associated documentation files (the
@@ -26,7 +26,8 @@
 
 ;; Folder structure
 ;; . weeb
-;; `-- tensei-shitara-slime-datta-ken-2nd-season
+;; |-- search-that time
+;  `-- anime-tensei-shitara-slime-datta-ken-2nd-season
 
 ;;; Code:
 
@@ -43,17 +44,31 @@
   :type 'string
   :group 'weeb)
 
-(defcustom weeb-db-dir (format "%s/.cache/%s" (getenv "HOME") "weeb")
-  "Directory where all searched animes are store for better performance."
-  :type 'string
-  :group 'weeb)
-
 (defcustom weeb-mpv-video-command "mpv --http-header-fields=\"Referer: %s\" %s >/dev/null"
   "Mpv command to run when a video is found."
   :type 'string
   :group 'weeb)
 
+(defcustom weeb-older-than (* 60 60 24)
+  "When weeb considers a file old enough to be replaced.")
+
 ;;; Helpers
+
+;; TODO: Make it async
+(defmacro scrape-from-uri (uri &rest body)
+  "Macro to scrape from uri"
+  `(let ((ssl-opt (if (string= (substring ,uri 0 5) "https") "-k" "")))
+     (with-temp-buffer
+       (call-process "curl" nil t nil "-s" "-A" "Mozilla/5.0" ssl-opt ,uri)
+       (goto-char (point-min))
+       ,@body)))
+
+(defun weeb--list-from-hash (key1 key2 hash)
+  "List from hash"
+  (let ((result '()))
+    (dolist (obj hash)
+      (push `(,(gethash key1 obj) ,(gethash key2 obj)) result))
+    result))
 
 (defun weeb--anime-field-accessor (field anime)
   "Anime field accessor."
@@ -75,109 +90,160 @@
   "Mpv command constructor"
   (format weeb-mpv-video-command referer video-url))
 
-(defun weeb-selected ()
-  "Find selected entry"
-  (let* ((episodes-total (length weeb-anime-episodes))
-         (episode-number (- (line-number-at-pos (point)) weeb-anime-offset))
-         (within-limits (and (>= episode-number 0) (< episode-number episodes-total))))
-    (when (and within-limits (arrayp weeb-anime-episodes))
-      (aref weeb-anime-episodes episode-number))))
+;; CRUD
 
-(defvar weeb-anime-episodes nil
-  "Anime episodes")
+(defcustom weeb-db-dir (format "%s/.cache/%s" (getenv "HOME") "weeb")
+  "Directory where all searched animes are store for better performance."
+  :type 'string
+  :group 'weeb)
 
-(defvar weeb-anime-offset 9
-  "Offset list")
+(defun weeb--db-insert (file-path object)
+  "DB insert."
+  (unless (file-exists-p file-path)
+    (with-temp-buffer
+      (json-insert object)
+      (write-file file-path nil))))
 
-(defvar weeb-anime-card-regex "<a href=\"/category/\\(.+\\)\" title=\"\\(.+\\)\".+</a>"
-  "Regex to find all animes.")
+(defun weeb--db-exists-p (file-path)
+  "DB exists predicate."
+  (info-file-exists-p file-path))
 
-(defvar weeb-anime-title-regex "<h1.*>\\(.*\\)</h1>"
-  "Regex to find anime title.")
+(defun weeb--db-select (file-path)
+  "DB select."
+  (when (file-exists-p file-path)
+    (let* ((json-object-type 'hash-table)
+           (json-array-type 'list)
+           (json-key-type 'string))
+          (json-read-file file-path))))
 
-(defvar weeb-anime-type-regex "<a href=\"/sub-category/.*\">\\(.*\\)</a>"
-  "Regex to find anime type.")
+(defun weeb--db-update (file-path new-object)
+  "DB update."
+  (weeb--db-delete file-path)
+  (weeb--db-insert file-path new-object))
 
-(defvar weeb-anime-summary-regex "<p class=\"type.+><span>Plot Summary.*</span>\\(.*\\)"
-  "Regex to find anime summary.")
+(defun weeb--db-delete (file-path)
+  "DB delete."
+  (when (file-exists-p file-path)
+    (delete-file file-path)))
 
-;; TODO: Find a better way to identify each genre
-(defvar weeb-anime-genre-regex "<a href=\"https\:.*/genre/.*\" title=\"\\(action\\|adventure\\)\">\\(action\\|adventure\\)</a>"
-  "Regex to find anime genre")
+(defun weeb--db-is-old-p (file-path)
+  "DB is older predicate."
+  (let* ((time (time-convert (current-time) 'integer))
+        (file-time (time-convert
+                    (file-attribute-modification-time
+                     (file-attributes file-path 'string)) 'integer))
+        (difference (- time file-time)))
+    (> difference weeb-older-than)))
 
-(defvar weeb-anime-released-date-regex "<p class=\"type.+>Released.*</span>\\(.*\\)</p>"
-  "Regex to find anime released date.")
-
-(defvar weeb-anime-status-regex "<a href=\"/\\(completed\\|ongoing\\).*>\\(.*\\)</a>"
-  "Regex to find anime status")
-
-(defvar weeb-anime-episodes-range-regex "<a href=.+ class=.+ ep_start = '\\([0-9]*\\)' ep_end = '\\([0-9]*\\)'>.+</a>"
-  "Regex to find anime episodes range.")
-
-(defvar weeb-anime-video-url-regex "<a href=\"#\" rel=\"100\" data-video=\"\\(.+\\)\" >.+</a>"
-  "Regex to find the video url for the selected anime.")
-
-(defvar weeb-inner-episode-video-url-regex "sources:\\[{file: '\\(.+\\)',label.+"
-  "Inner episode video regex.")
-
-;;; DB
-
-(defun weeb--db-file-setter (file-path slug object)
-  "DB file setter"
-  (with-temp-buffer
-    (json-insert object)
-    (write-file file-path nil)))
-
-(defun weeb--db-file-getter (file-path)
-  "DB file getter"
-  (json-read-file file-path))
-
-(defun weeb--db-handler (slug)
-  "DB file handler"
+(defun weeb--db-handler (prefix action slug &optional object)
+  "DB handler."
   (unless (file-directory-p weeb-db-dir)
     (make-directory weeb-db-dir))
-  (let ((file-path (format "%s/%s" weeb-db-dir slug)))
-    (unless (file-exists-p file-path)
-      (weeb--db-file-setter file-path slug (weeb--search-anime-by-slug slug)))
-    (weeb--db-file-getter file-path)))
+  (let ((file-path (format "%s/%s-%s" weeb-db-dir prefix slug)))
+    (cond
+     ((string= action "insert") (weeb--db-insert file-path object))
+     ((string= action "select") (weeb--db-select file-path))
+     ((string= action "update") (weeb--db-update file-path object))
+     ((string= action "delete") (weeb--db-delete file-path))
+     ((string= action "exists") (weeb--db-exists-p file-path))
+     ((string= action "older") (weeb--db-is-old-p file-path))
+     (t (error "unknown action: %s" action)))))
+
+;;; Repository
+
+;; Anime
+
+(defun weeb--repository-insert-anime (slug anime)
+  "Insert anime."
+  (weeb--db-handler "anime" "insert" slug anime))
+
+(defun weeb--repository-exists-anime-p (slug)
+  "Check anime."
+  (weeb--db-handler "anime" "exists" slug))
+
+(defun weeb--repository-is-old-anime-p (slug)
+  "Check if anime is old."
+  (weeb--db-handler "anime" "older" slug))
+
+(defun weeb--repository-get-anime (slug)
+  "Get anime."
+  (weeb--db-handler "anime" "select" slug))
+
+(defun weeb--repository-update-anime (slug anime)
+  "Update anime."
+  (weeb--db-handler "anime" "update" slug anime))
+
+(defun weeb--repository-delete-anime (slug)
+  "Delete anime."
+  (weeb--db-handler "anime" "delete" slug))
+
+;; Search
+
+(defun weeb--repository-insert-search (keyword search)
+  "Insert search."
+  (weeb--db-handler "search" "insert" keyword search))
+
+(defun weeb--repository-exists-search-p (keyword)
+  "Check search."
+  (weeb--db-handler "search" "exists" keyword))
+
+(defun weeb--repository-is-old-search-p (keyword)
+  "Check if search is old."
+  (weeb--db-handler "search" "older" keyword))
+
+(defun weeb--repository-get-search (keyword)
+  "Get search."
+  (weeb--db-handler "search" "select" keyword))
+
+(defun weeb--repository-update-search (keyword search)
+  "Update search."
+  (weeb--db-handler "search" "update" keyword search))
+
+(defun weeb--repository-delete-search (keyword)
+  "Delete search."
+  (weeb--db-handler "search" "delete" keyword))
 
 ;;; HTTP
 
-(defun weeb--search-anime-by-keyword (keyword)
+(defun weeb--http-search-anime-by-keyword (keyword)
   "Search anime by keyword."
   (let ((animes (list)))
     (set-buffer (url-retrieve-synchronously (weeb-search-url keyword)))
-    (while (re-search-forward weeb-anime-card-regex nil t)
-      (push `(,(match-string 2) . ,(match-string 1)) animes))
-    animes))
+    (while (re-search-forward "<a href=\"/category/\\(.+\\)\" title=\"\\(.+\\)\".+</a>" nil t)
+      (push `((title . ,(match-string 2))
+              (slug . ,(match-string 1))) animes))
+    `((keyword . ,keyword)
+      (result . [,@animes]))))
 
-(defun weeb--search-anime-by-slug (slug)
-  "Fetch single anime by slug."
+(defun weeb--http-get-single-anime (slug)
+  "Get single anime by slug."
   (let* ((buffer (url-retrieve-synchronously (weeb-single-anime-url slug)))
          (title (weeb--search-anime-title buffer))
          (type (weeb--search-anime-type buffer))
          (summary (weeb--search-anime-summary buffer))
          ;; FIXME: find a way to parse it correctly
-         (genres (weeb--search-anime-genres buffer))
          (released-date (weeb--search-anime-released-date buffer))
          (status (weeb--search-anime-status buffer))
          (episodes-range (weeb--search-anime-episodes-range buffer))
-         (first-episode (1+ (car episodes-range)))
          (last-episode (cadr episodes-range))
-         (episodes (weeb--episodes-list slug first-episode last-episode)))
+         (episodes (weeb--episodes-list slug last-episode)))
     `((title . ,title)
       (slug . ,slug)
       (type . ,type)
       (summary . ,summary)
       (released-date . ,released-date)
       (status . ,status)
-      (first-episode . ,first-episode)
       (last-episode . ,last-episode)
       (episodes . [,@episodes]))))
 
-(defun weeb--episodes-list (slug start end)
+(defun weeb--episodes-list (slug end)
   "Construct episodes path"
-  (mapcar (apply-partially #'weeb-anime-episode-url slug) (number-sequence start end)))
+  (let ((result '()))
+    (dotimes (episode end)
+      (push `((title . ,(format "Episode %d" (1+ episode)))
+              (episode . ,(1+ episode))
+              (slug . ,(weeb-anime-episode-url slug (1+ episode)))) result))
+    result))
 
 (defun weeb--search-anime-title (buffer)
   "Search anime title"
@@ -194,165 +260,92 @@
 (defun weeb--search-anime-summary (buffer)
   "Search anime summary"
   (set-buffer buffer)
-  (re-search-forward weeb-anime-summary-regex nil t)
+  (re-search-forward "<p class=\"type.+><span>Plot Summary.*</span>\\(.*\\)" nil t)
   (match-string 1))
 
+;; FIXME: Does not get all genres
 (defun weeb--search-anime-genres (buffer)
   "Search anime genres"
   (set-buffer buffer)
-  (re-search-forward weeb-anime-genre-regex nil t)
+  (re-search-forward "<a href=\"https\:.*/genre/.*\" title=\"\\(action\\|adventure\\)\">\\(action\\|adventure\\)</a>" nil t)
   (match-string 1))
 
 (defun weeb--search-anime-released-date (buffer)
   "Search anime released date"
   (set-buffer buffer)
-  (re-search-forward weeb-anime-released-date-regex nil t)
+  (re-search-forward "<p class=\"type.+>Released.*</span>\\(.*\\)</p>" nil t)
   (match-string 1))
 
 (defun weeb--search-anime-status (buffer)
   "Search anime status"
   (set-buffer buffer)
-  (re-search-forward weeb-anime-status-regex nil t)
+  (re-search-forward "<a href=\"/\\(completed\\|ongoing\\).*>\\(.*\\)</a>" nil t)
   (match-string 2))
 
 (defun weeb--search-anime-episodes-range (buffer)
   "Search anime episodes range."
   (let ((ranges '()))
     (set-buffer buffer)
-    (while (re-search-forward weeb-anime-episodes-range-regex nil t)
+    (while (re-search-forward "<a href=.+ class=.+ ep_start = '\\([0-9]*\\)' ep_end = '\\([0-9]*\\)'>.+</a>" nil t)
       (setq ranges (append `(,(string-to-number (match-string 1)) ,(string-to-number (match-string 2))) ranges)))
     `(,(apply #'min ranges) ,(apply #'max ranges))))
 
 (defun weeb--search-anime-episode-video-url (url)
   "Search episode video url for selected anime."
   (set-buffer (url-retrieve-synchronously url))
-  (re-search-forward weeb-anime-video-url-regex nil t)
+  (re-search-forward "<a href=\"#\" rel=\"100\" data-video=\"\\(.+\\)\" >.+</a>" nil t)
   (format "https:%s" (match-string 1)))
 
 (defun weeb--search-anime-episode-inner-video-url (url)
   "Search inner episode video url to be played in mpv."
   (set-buffer (url-retrieve-synchronously url))
-  (re-search-forward weeb-inner-episode-video-url-regex nil t)
+  (re-search-forward "sources:\\[{file: '\\(.+\\)',label.+" nil t)
   (match-string 1))
 
-;;; Format
+;;; Service
 
-(defun weeb--anime-title-format (text)
-  ""
-  (insert (format "Title: %s\n" text)))
+(defun weeb--service-search-anime-by-keyword (keyword)
+  "Search anime by keyword."
+  (cond
+   ((not (weeb--repository-exists-search-p keyword))
+    (weeb--repository-insert-search keyword (weeb--http-search-anime-by-keyword keyword)))
+   ((weeb--repository-is-old-search-p keyword)
+    (weeb--repository-update-search keyword (weeb--http-search-anime-by-keyword keyword))))
+  (weeb--repository-get-search keyword))
 
-(defun weeb--anime-type-format (text)
-  ""
-  (insert (format "Type: %s\n" text)))
-
-(defun weeb--anime-episodes-format (text)
-  ""
-  (insert (format "Episodes: %d\n" text)))
-
-(defun weeb--anime-released-date-format (text)
-  ""
-  (insert (format "Released Date: %s\n" text)))
-
-(defun weeb--anime-status-format (text)
-  ""
-  (insert (format "Status: %s\n" text)))
-
-(defun weeb--anime-summary-format (text)
-  ""
-  (insert (format "Summary: %s\n" text)))
-
-(defun weeb--anime-genres-format (text)
-  ""
-  (insert (format "Genres: %s\n" text)))
-
-(defun weeb--anime-slug-format (text)
-  ""
-  (insert (format "Slug: %s\n" text)))
-
-(defun weeb--anime-episode-format (episode)
-  "Episode list format."
-  (insert (propertize (format "Episode %d\n" episode) 'face 'keyword)))
-
-(defun weeb--anime-episode-entries (start end)
-  "Episode list generator"
-  (mapcan #'weeb--anime-episode-format (number-sequence start end)))
-
-(defun weeb--mode-view (anime)
-  "Render `weeb-mode' since it is not attached to a extension"
-  (let* ((title (weeb--anime-field-accessor "title" anime))
-         (status (weeb--anime-field-accessor "status" anime))
-         (type (weeb--anime-field-accessor "type" anime))
-         (released-date (weeb--anime-field-accessor "released-date" anime))
-         (summary (weeb--anime-field-accessor "summary" anime))
-         (slug (weeb--anime-field-accessor "slug" anime))
-         (first-episode (weeb--anime-field-accessor "first-episode" anime))
-         (last-episode (weeb--anime-field-accessor "last-episode" anime))
-         (episodes (weeb--anime-field-accessor "episodes" anime))
-         (buffer (get-buffer-create (format "*Weeb: %s*" title))))
-    (with-current-buffer buffer
-      (weeb--anime-title-format title)
-      (weeb--anime-status-format status)
-      (weeb--anime-type-format type)
-      ;; (weeb--anime-genres-format genres)
-      (weeb--anime-released-date-format released-date)
-      (weeb--anime-slug-format slug)
-      (weeb--anime-episodes-format last-episode)
-      (weeb--anime-summary-format summary)
-      (newline)
-      (weeb--anime-episode-entries first-episode last-episode)
-      (goto-char (point-min))
-      (weeb-mode)
-      (setf weeb-anime-episodes episodes)
-      (switch-to-buffer buffer))))
-
-;;; Mode
+(defun weeb--service-get-single-anime (slug)
+  "Get anime by slug"
+  (cond
+   ((not (weeb--repository-exists-anime-p slug)) (weeb--repository-insert-anime slug (weeb--http-get-single-anime slug)))
+   ((weeb--repository-is-old-anime-p slug) (weeb--repository-update-anime slug (weeb--http-get-single-anime slug))))
+  (weeb--repository-get-anime slug))
 
 (defun weeb-watch-episode (episode)
-  "Watch the currently selected entry."
-  (interactive (list (weeb-selected)))
-  (when episode
-    (let* ((referer (weeb--search-anime-episode-video-url episode))
-           (video-url (weeb--search-anime-episode-inner-video-url referer)))
-      (start-process-shell-command "weeb-mpv" nil (weeb-mpv-command referer video-url)))))
+  "Watch selected episode."
+  (let* ((slug (cadr episode))
+         (referer (weeb--search-anime-episode-video-url slug))
+         (video-url (weeb--search-anime-episode-inner-video-url referer)))
+    (start-process-shell-command "weeb-mpv" nil (weeb-mpv-command referer video-url))
+    (message "%s sent to mpv" slug)))
 
+(defun weeb-select-episode (anime)
+  (let* ((title (car anime))
+         (slug (cadr anime))
+         (episodes (gethash "episodes" (weeb--service-get-single-anime slug)))
+         (episodes-list (weeb--list-from-hash "title" "slug" episodes))
+         (choice (completing-read (format "Choose %s Episode: " title) episodes-list nil t))
+         (episode-selected (assoc-string choice episodes-list)))
+    (weeb-watch-episode episode-selected)))
+  
+;;;###autoload
 (defun weeb-search-anime (keyword)
   "Search anime by keyword."
   (interactive "sWeeb Search Anime: ")
-  (if (string= keyword "")
-      (message "TODO: fetching latest episodes not implemented")
-    (let* ((animes (weeb--search-anime-by-keyword keyword))
-           (anime-selected (assoc-string (completing-read "Choose Anime: " animes nil t)
-                                         animes t))
-           (slug (cdr anime-selected))
-           (anime (weeb--db-handler slug)))
-      (weeb--mode-view anime))))
-
-(defvar weeb-mode-map
-  (let ((map (make-sparse-keymap))
-        (menu-map (make-sparse-keymap "Weeb")))
-    (define-key map (kbd "C-p") #'previous-line)
-    (define-key map (kbd "p") #'previous-line)
-    (define-key map (kbd "C-n") #'next-line)
-    (define-key map (kbd "n") #'next-line)
-    (define-key map (kbd "s") #'weeb-search-anime)
-    (define-key map (kbd "RET") #'weeb-watch-episode)
-    (define-key map (kbd "w") #'weeb-watch-episode)
-    map)
-  "Keymap for `weeb-mode'.")
-
-(defvar weeb-font-lock-keywords
-  '(("Title\\|Episodes\\|Status\\|Type\\|Genres\\|Summary\\|Released Date\\|Slug" . 'font-lock-constant-face)
-    ("Episode" . 'font-lock-keyword-face))
-  "Keyword highlighting specification for `weeb-mode'.")
-
-(defvar weeb-mode-hook nil
-  "Hook for `weeb-mode'.")
-
-;;;###autoload
-(define-derived-mode weeb-mode special-mode "Weeb"
-  "Definition for `weeb-mode'."
-  (make-variable-buffer-local 'weeb-anime-episodes)
-  (setq-local font-lock-defaults '(weeb-font-lock-keywords)))
+  (let* ((result (weeb--service-search-anime-by-keyword keyword))
+         (animes (weeb--list-from-hash "title" "slug" (gethash "result" result)))
+         (choice (completing-read "Choose Anime: " animes nil t))
+         (anime-selected (assoc-string choice animes)))
+    (weeb-select-episode anime-selected)))
 
 (provide 'weeb)
 ;;; weeb.el ends here
