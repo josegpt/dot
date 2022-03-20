85d27bd4925c08a61b80028c0003ad9f4c80a43b
Jose G Perez Taveras <josegpt27@gmail.com>
2022-03-20 01:15:28 -0400

    emacs[dired]: dired-use-ls-dired t -> nil

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -70,6 +70,7 @@
 
 (setup dired
   (:option dired-dwim-target t
+           dired-use-ls-dired nil
            dired-listing-switches "-alh"
            dired-kill-when-opening-new-dired-buffer t))
 
