8f46b51dfe78f1a1a2f97f04748bbe93296b115b
Jose G Perez Taveras <josegpt27@gmail.com>
2022-04-18 20:48:57 -0400

    emacs: remove modus-themes

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -208,8 +208,6 @@ _\\/_            |      o/      _\\/_
 (setup man
   (:global "C-c m" #'man))
 
-(setup (:package modus-themes)
-  (load-theme 'modus-vivendi t))
 
 (setup show-paren-mode
   (:option show-paren-when-point-inside-paren t
